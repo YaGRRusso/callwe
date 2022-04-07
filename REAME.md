be8795cf7bf21ed6ab73279a93e7e4e91d6fecfc
Yago Russo <yagrrusso@gmail.com>
2022-04-07 16:27:33 -0300

    finalizado

---
+++ REAME.md
@@ -0,0 +1,22 @@
+# Callwe
+Landing page responsiva e funcional criada a partir do design fornecido no desafio.
+
+## Bem-Vindo!
+
+Funcionalidades Requeridas
+- Responsividade;
+- Autopreenchimento de endereço através de API de CEP (ViaCEP);
+- Utilizar apenas HTML, CSS e JS;
+- Carousel;
+
+Funcionalidades Extras
+- Menu mobile (visível em telas menores);
+- Botão "ver artigo" funcional;
+- Vídeos são reproduzidos ao clicar no poster;
+- Animações em geral;
+- Dados do formulário salvos e prontos para POST;
+
+### Observações
+Como não me foi fornecido nenhuma imagem, icone, logo, svg ou vídeo, utilizei material da internet.
+A imagem principal da página é gerada de forma aleatória através do site https://picsum.photos/,
+logos, icones e vídeos foram pegos do Google apenas para serem utilizados como exemplo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
+// Dados da api ViaCEP (podem ser trocados por outra API sem afetar o funcionamento do site)
 const BaseURL = 'https://viacep.com.br/ws/';
-
 const api = {
     getCep: async (cep) => {
         let response = await fetch(BaseURL + cep + '/json');
@@ -8,6 +8,7 @@ const api = {
     }
 };
 
+// Menu mobile (visível em telas com menos de 1024px)
 document.querySelector('.menu-hamb-area').addEventListener('click', () => {
     document.querySelector('.menu-modal').classList.toggle('active');
     document.querySelector('.nav-menu').classList.toggle('active');
@@ -15,9 +16,11 @@ document.querySelector('.menu-hamb-area').addEventListener('click', () => {
     document.querySelector('.menu-hamb').classList.toggle('active');
 });
 
+// Botão 'VER ARTIGOS"
 document.querySelectorAll('.service div').forEach((item) => {
     item.addEventListener('click', (ev) => {
         if (document.querySelector('.service div.active')) {
+            // Se o artigo selecionado já estiver ativo, ele será desativado ao clicar novamento no mesmo lugar
             if (document.querySelector('.service div.active') === ev.currentTarget) {
                 document.querySelector('.service div.active').classList.remove('active');
             } else {
@@ -30,6 +33,7 @@ document.querySelectorAll('.service div').forEach((item) => {
     })
 })
 
+// Reprodução de vídeo
 document.querySelectorAll('video').forEach((item) => {
     item.addEventListener('click', (ev) => {
         ev.target.play()
@@ -37,8 +41,11 @@ document.querySelectorAll('video').forEach((item) => {
     })
 })
 
+// Inicialização do carousel
 let translateCount = 0;
 let slideCount = 0;
+
+// Contagem de slides
 const writeCarousel = () => {
     let span = '<span data-slide="0" class="active"></span>'
     for (let i = 1; i < slideCount; i++) {
@@ -47,18 +54,21 @@ const writeCarousel = () => {
     document.querySelector('.carousel-count').innerHTML = span
 }
 
+// Verificação do slide atual
 const setCurrentSlide = () => {
     document.querySelector('.carousel-count span.active').classList.remove('active')
     document.querySelector(`[data-slide='${translateCount}']`).classList.add('active')
 }
 
+// Divisão de vídeos em slides baseado no tamanho da tela (responsividade do carousel)
 const carouselItems = document.querySelectorAll('.carousel-item').length
 const setTranslateCarousel = (value) => {
     const windowWidth = window.innerWidth;
     let translatePercent = 0
     translateCount += value
 
-    translateCount < 0 ? translateCount = 0 : false
+    // Impede o carousel de bugar quando os slides não são completos (3 espaços e 2 vídeos = 0.66)
+    translateCount < 0 ? translateCount = Math.ceil(slideCount - 1) : false
 
     if (windowWidth > 768) {
         slideCount = carouselItems / 3;
@@ -71,6 +81,7 @@ const setTranslateCarousel = (value) => {
         translatePercent = 100;
     }
 
+    // Checagem de reinício de carousel
     if (translateCount < 0) {
         translateCount = slideCount - 1
     } else if (translateCount >= slideCount) {
@@ -83,13 +94,13 @@ const setTranslateCarousel = (value) => {
     return translateCount * translatePercent
 }
 
+// Botões do carousel
 document.querySelector('#carousel-prev').addEventListener('click', () => {
     const translateValue = setTranslateCarousel(-1)
     document.querySelectorAll('.carousel-item').forEach((item) => {
         item.style.transform = `translateX(-${translateValue}%)`
     })
 })
-
 document.querySelector('#carousel-next').addEventListener('click', () => {
     const translateValue = setTranslateCarousel(+1)
     document.querySelectorAll('.carousel-item').forEach((item) => {
@@ -97,6 +108,7 @@ document.querySelector('#carousel-next').addEventListener('click', () => {
     })
 })
 
+// Formulário
 const formName = document.querySelector('#form-cep');
 const formEmail = document.querySelector('#form-cep');
 const formTel = document.querySelector('#form-cep');
@@ -106,9 +118,11 @@ const formNumber = document.querySelector('#form-number');
 const formCity = document.querySelector('#form-city');
 const formState = document.querySelector('#form-state');
 
+// Executa a API ao tirar o foco do campo CEP (evita requisições desnecessárias à API)
 formCep.addEventListener('focusout', async (item) => {
     const cep = item.target.value;
     let regexCep = /[0-9]{8}/;
+    // Evita requisição com CEPS que contém letras
     if (regexCep.test(cep)) {
         const cepInfo = await api.getCep(cep);
         if (!cepInfo.erro) {
@@ -125,6 +139,7 @@ formCep.addEventListener('focusout', async (item) => {
     }
 })
 
+// Separa os dados do formulário em uma variável para um possível POST
 document.querySelector('#user-form').addEventListener('submit', (ev) => {
     ev.preventDefault();
     const personalData = {
@@ -137,10 +152,10 @@ document.querySelector('#user-form').addEventListener('submit', (ev) => {
         city: formCity.value,
         state: formState.value
     };
-    alert('Dados no console!')
     console.log(personalData)
 })
 
+// Inicialização de funções fundamentais
 writeCarousel();
 setTranslateCarousel(0)
 setCurrentSlide()
